2bc76ab9b17ba29562c962845c1bf7dd987b05a1
Myla Voronevska <code.crystal.008@gmail.com>
2023-06-27 12:11:23 +0300

    fixed deploying

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Wanderlust Adventures</title>
-
   <link rel="icon" type="image/png" href="images/logo.png" sizes="32x32">
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Pacifico&display=swap" rel="stylesheet">
 
+  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
   <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
