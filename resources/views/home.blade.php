af64bc4b0aeb421445a89ffca5438b95a6309984
Myla Voronevska <code.crystal.008@gmail.com>
2023-06-27 11:46:03 +0300

    title fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>
-    Wanderlust Adventures - Tourism Agency
-  </title>
+  <title>Wanderlust Adventures - Tourism Agency</title>
 
   <link rel="icon" type="image/png" href="images/logo.png" sizes="32x32">
 
