fce2209dc164b9b3a0dbbf0e5ab6af02fa7a478a
Myla Voronevska <code.crystal.008@gmail.com>
2023-06-27 12:14:05 +0300

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,35 +14,6 @@
   <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
   <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-
-  <style>
-    .swiper-pagination-bullet {
-      opacity: 0.7;
-      background-color: white;
-      border: 1px solid #333;
-    }
-
-    .swiper-pagination-bullet-active {
-      background-color: #333;
-      border: 1px solid white;
-    }
-
-    .bgc-1 {
-      background: linear-gradient(to left, #3b5998, #6792c7, #96b5e1, #c7d9f5);
-    }
-
-    .bgc-2 {
-      background: linear-gradient(to right, #a3d9ff, #92c8f9, #aab5e7, #c997d4, #e378c2, #f864b0, #ff868c);
-    }
-
-    .bgc-3 {
-      background: linear-gradient(to right, #ff9999 25%, #ff8080 40%, #ff6666 55%, #ff4d4d 70%, #ff4d4d);
-    }
-
-    .bgc-4 {
-      background: linear-gradient(to left, #73c7ff, #87d0ff, #9bdcff, #a9e4ff, #b6ecff, #c3f3ff);
-    }
-  </style>
 </head>
 
 <body>
